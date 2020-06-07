d81af74c55991a2f88e78c3147eb53994a78e7af
Suraj Nair <surajnair.caltech@gmail.com>
2020-06-07 12:23:37 -0700

    cleaning up experiment code

--- exec_loop.py
+++ exec_loop.py
@@ -24,78 +24,84 @@ class TempEnv():
 def make_url(node, endpoint=None):
     return "http://{}:{}/{}".format("localhost", node, endpoint)
 
-
+async def get_response(session, url, data):
+    try:
+        result = await session.get(url, json=data)
+        js = await result.json()
+        return(js['res'])
+    except:
+        print("DONE!")
+  
 async def send_msg(session, url, data):
     try:
-        await session.post(url, json=data)
+        result = await session.post(url, json=data)
     except:
         print("DONE!")
 
 
-def exec_loop(num_agents, num_eps):
-    """
-    Runs loop
-    """
+def main():
+    parser = argparse.ArgumentParser(description='PBFT Node')
+    parser.add_argument('-n', '--num_agents', type=int, help='node index')
+    parser.add_argument('--num_eps', type=int, help='node index')
+    parser.add_argument('--num_faulty', type=int, help='node index')
+    #parser.add_argument('-env', '--env', type=str, help='domain')
+    args = parser.parse_args()
+
     env = TempEnv()
 
     subprocess.run("rm ./logs/agent*", shell=True)
     print("DELETED OLD FILES")
 
-    for i in range(num_agents):
+    for i in range(args.num_agents):
         # subprocess.run(f"fuser -k {30000+i}/tcp", shell=True)
         subprocess.run(f"lsof -nti:{30000+i} | xargs kill -9", shell=True)
 
     print("KILLED OLD PROC")
     p = []
-    for i in range(num_agents):
-        p.append(Popen(f"python ./run_agent.py -i {i} -n {num_agents} -f 2", shell=True))
+    for i in range(args.num_agents):
+        p.append(Popen(f"python ./run_agent.py -i {i} -n {args.num_agents} -f {args.num_faulty}", shell=True))
     print("STARTED NEW PROC")
     time.sleep(10)
 
     timeout = aiohttp.ClientTimeout(1000)
     sess = aiohttp.ClientSession(timeout=timeout)
 
-    for ep in range(num_eps):
+    true_obs = []
+    for ep in range(args.num_eps):
         print("EPISODE:", ep)
         obs = env.reset()
+        true_obs.append([obs])
         loop = asyncio.get_event_loop()
-        futures = [send_msg(sess, make_url(30000 + i, "setobs"), {"obs": obs}) for i in range(num_agents)]
+        futures = [send_msg(sess, make_url(30000 + i, "setobs"), {"obs": obs}) for i in range(args.num_agents)]
         loop.run_until_complete(asyncio.gather(*futures))
 
         loop = asyncio.get_event_loop()
-        futures = [send_msg(sess, make_url(30000 + i, "send_obs_request"), {}) for i in range(num_agents)]
+        futures = [send_msg(sess, make_url(30000 + i, "send_obs_request"), {}) for i in range(args.num_agents)]
         loop.run_until_complete(asyncio.gather(*futures))
 
         loop = asyncio.get_event_loop()
-        futures = [send_msg(sess, make_url(30000 + i, "reopen"), {}) for i in range(num_agents)]
+        futures = [send_msg(sess, make_url(30000 + i, "reopen"), {}) for i in range(args.num_agents)]
         loop.run_until_complete(asyncio.gather(*futures))
-    #         time.sleep(1)
-    sess.close()
+        
+    loop = asyncio.get_event_loop()
+    futures = [get_response(sess, make_url(30000 + i, "get_results"), {}) for i in range(args.num_agents)]
+    rs = loop.run_until_complete(asyncio.gather(*futures))
+    results = np.array(rs)
+    true_obs = np.array(true_obs)
+    print(true_obs.shape)
+    true_obs = true_obs.repeat(args.num_agents, 1)
+    print(true_obs.shape)
+    results = results.swapaxes(0, 1)
+    print(results.shape)
+    results = np.stack([results, true_obs])
+    print(results.shape)
+    np.save(f"logs/results_nagents_{args.num_agents}_numeps_{args.num_eps}_nfaulty_{args.num_faulty}_type_random.npy", results)
+        
+    asyncio.ensure_future(sess.close())
     time.sleep(3)
     for pr in p:
         pr.terminate()
     print("KILLED NEW PROC")
-
-
-def main():
-    """
-    Run experiments
-    """
-    parser = argparse.ArgumentParser(description='PBFT Node')
-    parser.add_argument('-n', '--num_agents', type=int, help='node index')
-    parser.add_argument('--num_eps', type=int, help='node index')
-    parser.add_argument('--expt_type', type=str, help='num_agents, num_fagents')
-    #parser.add_argument('-env', '--env', type=str, help='domain')
-    args = parser.parse_args()
-
-    exec_loop(args.num_agents, args.num_eps)
-    #if args.expt_type == 'num_agents':
-    #    for num in range(1, args.num_agents+1):  # want to start from 1, not 0
-    #        print("\nNUM AGENTS: ", num)
-    #        exec_loop(num, args.num_eps)
-
-
-
 
 
 if __name__ == "__main__":
